1449ac3af3c942695ee2d9077440756deb440c3a
Munieru <20086673+munierujp@users.noreply.github.com>
2019-08-09 10:42:11 +0900

    Add Firebase Authentication (#2)
    * Add firebase package
    
    `npm i -D firebase`
    
    * Add firebase.config.js
    
    * Add firebase.js
    
    * Update index page

---
+++ firebase.config.js
@@ -0,0 +1,10 @@
+// NOTE: It's safe to publish these values.
+export default {
+  apiKey: 'AIzaSyDtYgrNF784HQeHA9fKdR9utHmD8OSerdY',
+  authDomain: 'nuxt-firebase-authentication.firebaseapp.com',
+  databaseURL: 'https://nuxt-firebase-authentication.firebaseio.com',
+  projectId: 'nuxt-firebase-authentication',
+  storageBucket: '',
+  messagingSenderId: '570149189200',
+  appId: '1:570149189200:web:ab17df9d6094c44e'
+}
